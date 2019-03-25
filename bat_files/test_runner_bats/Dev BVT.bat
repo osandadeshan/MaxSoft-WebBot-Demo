1d02e2d19036dca759e4991ee0db23491083e829
Osanda Deshan <osanda.deshan@gmail.com>
2019-03-25 21:23:12 +0530

    Initial commit

---
+++ bat_files/test_runner_bats/Dev BVT.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Executing MaxSoft WebBot Demo Test Automation Suite.............
+
+cd ..
+cd ..
+
+call mvn test-compile gauge:execute -DspecsDir="specs/search.spec" -Denv="dev"
+echo Exit Code = %ERRORLEVEL%
+if not "%ERRORLEVEL%" == "0" exit /b
+
+call mvn clean -DemailConfigEnv=dev install exec:java
+echo Exit Code = %ERRORLEVEL%
+if not "%ERRORLEVEL%" == "0" exit /b
---
+++ src/test/java/com/maxsoft/webbot/EmailSender.java
@@ -0,0 +1,23 @@
+package com.maxsoft.webbot;
+
+import com.maxsoft.webbot.util.email.Email;
+import com.maxsoft.webbot.util.email.JsonReportReader;
+
+/**
+ * Project Name : MaxSoft-WebBot-Demo
+ * Developer    : Osanda Deshan
+ * Version      : 1.0.0
+ * Date         : 24/03/2019
+ * Time         : 17:37
+ * Description  :
+ **/
+
+
+public class EmailSender {
+
+    public static void main(String[] args) {
+        Email.send(JsonReportReader.getExecutionResults());
+    }
+
+
+}
